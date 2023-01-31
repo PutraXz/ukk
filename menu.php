ffa903eba1b288a149a4e504d096dadd4e91097b
PutraXz <ytprem200100@gmail.com>
2023-01-31 14:20:08 +0700

    pp

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Data Siswa</a></li>
+        <li><a href="#">Data Petugas</a></li>
+        <li><a href="#">Data Spp</a></li>
+        <li><a href="#">Transaksi</a></li>
+        <li><a href="#">History Pembayaran</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#  " onclick="document.getElementById('id01').style.display='block'"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
