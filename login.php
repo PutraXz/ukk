b1203b74344f256c1f395bf847514a7bdb12e977
PutraXz <ytprem200100@gmail.com>
2023-01-31 14:28:08 +0700

    asd

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 
 <div id="id01" class="modal">
   <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="container">
+    <div class="container-fluid">
       <label for="uname"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="uname" required>
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>  
       <button type="submit">Login</button>
     </div>
-    <div class="container" style="background-color:#f1f1f1">
+    <div class="container-fluid" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
       <span class="psw">Forgot <a href="#">password?</a></span>
     </div>
